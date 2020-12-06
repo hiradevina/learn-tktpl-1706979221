d347df53743a9648e340b3dec8c3605bf9a2c0d3
Fakhira <fakhira.devina@ui.ac.id>
2020-12-06 19:36:27 +0700

    get data from .c

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/hira/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/hira/helloworld/MainActivity.kt
@@ -7,24 +7,35 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        jni_text.text = stringFromJNI()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+
+    external fun stringFromJNI(): String?
+
+    companion object {
+        /*
+         * this is used to load the 'hello-jni' library on application
+         * startup. The library has already been unpacked into
+         * /data/data/com.example.hellojni/lib/libhello-jni.so
+         * at the installation time by the package manager.
+         */
+        init {
+            System.loadLibrary("hello-jni")
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
